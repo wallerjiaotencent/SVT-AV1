986b24520e2c24681d511e82fcf8301ef96128b6
Colin Chan <colinchan02@gmail.com>
2019-11-08 08:59:12 -0800

    histogram sum was not being correctly reset back to 0

--- Source/Lib/Common/Codec/EbPictureDecisionProcess.c
+++ Source/Lib/Common/Codec/EbPictureDecisionProcess.c
@@ -3653,9 +3653,9 @@ void* picture_decision_kernel(void *input_ptr)
                                     // Accumulative histogram absolute differences between the central and future frame
                                     for (pic_itr = (index_center + actual_future_pics); pic_itr > index_center; pic_itr--) {
                                         ahd = 0;
-                                        center_histogram = 0;
-                                        altref_histogram = 0;
                                         for (int bin = 0; bin < HISTOGRAM_NUMBER_OF_BINS; ++bin) {
+                                            center_histogram = 0;
+                                            altref_histogram = 0;
                                             for (regionInPictureWidthIndex = 0; regionInPictureWidthIndex < sequence_control_set_ptr->picture_analysis_number_of_regions_per_width; regionInPictureWidthIndex++) {
                                                 for (regionInPictureHeightIndex = 0; regionInPictureHeightIndex < sequence_control_set_ptr->picture_analysis_number_of_regions_per_height; regionInPictureHeightIndex++) {
                                                     center_histogram += picture_control_set_ptr->temp_filt_pcs_list[index_center]->picture_histogram[regionInPictureWidthIndex][regionInPictureHeightIndex][0][bin];
@@ -3771,9 +3771,9 @@ void* picture_decision_kernel(void *input_ptr)
                                 for (pic_itr = index_center - actual_past_pics; pic_itr < index_center - 1; pic_itr++) {
 #endif
                                     ahd = 0;
-                                    center_histogram = 0;
-                                    altref_histogram = 0;
                                     for (int bin = 0; bin < HISTOGRAM_NUMBER_OF_BINS; ++bin) {
+                                        center_histogram = 0;
+                                        altref_histogram = 0;
                                         for (regionInPictureWidthIndex = 0; regionInPictureWidthIndex < sequence_control_set_ptr->picture_analysis_number_of_regions_per_width; regionInPictureWidthIndex++) {
                                             for (regionInPictureHeightIndex = 0; regionInPictureHeightIndex < sequence_control_set_ptr->picture_analysis_number_of_regions_per_height; regionInPictureHeightIndex++) {
                                                 center_histogram += picture_control_set_ptr->temp_filt_pcs_list[index_center]->picture_histogram[regionInPictureWidthIndex][regionInPictureHeightIndex][0][bin];
@@ -3791,9 +3791,9 @@ void* picture_decision_kernel(void *input_ptr)
                                 // Accumulative histogram absolute differences between the central and past frame
                                 for (pic_itr = (index_center + actual_future_pics); pic_itr > index_center; pic_itr--) {
                                     ahd = 0;
-                                    center_histogram = 0;
-                                    altref_histogram = 0;
                                     for (int bin = 0; bin < HISTOGRAM_NUMBER_OF_BINS; ++bin) {
+                                        center_histogram = 0;
+                                        altref_histogram = 0;
                                         for (regionInPictureWidthIndex = 0; regionInPictureWidthIndex < sequence_control_set_ptr->picture_analysis_number_of_regions_per_width; regionInPictureWidthIndex++) {
                                             for (regionInPictureHeightIndex = 0; regionInPictureHeightIndex < sequence_control_set_ptr->picture_analysis_number_of_regions_per_height; regionInPictureHeightIndex++) {
                                                 center_histogram += picture_control_set_ptr->temp_filt_pcs_list[index_center]->picture_histogram[regionInPictureWidthIndex][regionInPictureHeightIndex][0][bin];
